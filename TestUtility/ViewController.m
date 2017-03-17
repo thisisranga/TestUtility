7914bb32e711bed1d86bec476b15653fbd54d9f1
Ranga Reddy Nallavelli <thisisranga@gmail.com>
2017-03-17 12:22:55 -0400

    [Sorter TestUtility] Updating TestUtility to use [Sorter Utility].

--- TestUtility/ViewController.m
+++ TestUtility/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SorterMaster.h"
 
 @interface ViewController ()
 
@@ -14,9 +15,26 @@
 
 @implementation ViewController
 
+-(void)sorterExample {
+    SorterMaster *sorter = [[SorterMaster alloc] init];
+    NSMutableArray *unsortedArray = [NSMutableArray arrayWithArray:@[@63,@52,@103,@21,@45,@67,@106, @112, @1,@20,@37,@43]];
+//    NSMutableArray *unsortedArray = [NSMutableArray arrayWithArray:@[@"63",@"52",@"103",@"21",@"45",@"67",@"106", @"112", @"1",@"20",@"37",@"43"]];
+    dispatch_queue_t aQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(aQueue, ^{
+        NSArray *bubbleSortedArray = [sorter sortUsingBubbleSort:unsortedArray];
+        NSLog(@"bubble sorted Array = %@", bubbleSortedArray);
+        NSArray *selectionSortedArray = [sorter sortUsingSelectionSort:unsortedArray];
+        NSLog(@"selection sorted Array = %@", selectionSortedArray);
+        NSArray *mergeSortedArray = [sorter sortUsingMergeSort:unsortedArray];
+        NSLog(@"merge sorted Array = %@", mergeSortedArray);
+    });
+   
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self sorterExample];
 }
 
 - (void)didReceiveMemoryWarning {
